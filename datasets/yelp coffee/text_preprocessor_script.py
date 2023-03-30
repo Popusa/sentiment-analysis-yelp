464da849d43f6665affc099cf7ff4989a6353d43
popusa <danielmaged.3@gmail.com>
2023-03-30 06:39:53 +0200

    tweaked data prepation notebook and fixed some issues with model optimization notebook

--- datasets/yelp coffee/text_preprocessor_script.py
+++ datasets/yelp coffee/text_preprocessor_script.py
@@ -1,27 +1,23 @@
 ### IMPORTED LIBRARIES
+import numpy as np
 import pandas as pd
 import nltk
-from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import string
 from pathlib import Path
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import tkinter as tk
+from tkinter import simpledialog
 # nltk.download('punkt')
 # nltk.download('stopwords')
 # nltk.download('wordnet')
 # nltk.download('vader_lexicon')
 # nltk.download('averaged_perceptron_tagger')
+# nltk.download('omw-1.4')
 ### IMPORTED LIBRARIES
 
 
 ### GLOBAL VARIABLES
-MORE_STOP_WORDS = [
-"a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves,","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"
-]
-EVEN_MORE_STOP_WORDS = ["able","about","above","abroad","according","accordingly","across","actually","adj","after","afterwards","again","against","ago","ahead","ain't","all","allow","allows","almost","alone","along","alongside","already","also","although","always","am","amid","amidst","among","amongst","an","and","another","any","anybody","anyhow","anyone","anything","anyway","anyways","anywhere","apart","appear","appreciate","appropriate","are","aren't","around","as","a's","aside","ask","asking","associated","at","available","away","awfully","back","backward","backwards","be","became","because","become","becomes","becoming","been","before","beforehand","begin","behind","being","believe","below","beside","besides","best","better","between","beyond","both","brief","but","by","came","can","cannot","cant","can't","caption","cause","causes","certain","certainly","changes","clearly","c'mon","co","co.","com","come","comes","concerning","consequently","consider","considering","contain","containing","contains","corresponding","could","couldn't","course","c's","currently","dare","daren't","definitely","described","despite","did","didn't","different","directly","do","does","doesn't","doing","done","don't","down","downwards","during","each","edu","eg","eight","eighty","either","else","elsewhere","end","ending","enough","entirely","especially","et","etc","even","ever","evermore","every","everybody","everyone","everything","everywhere","ex","exactly","example","except","fairly","far","farther","few","fewer","fifth","first","five","followed","following","follows","for","forever","former","formerly","forth","forward","found","four","from","further","furthermore","get","gets","getting","given","gives","go","goes","going","gone","got","gotten","greetings","had","hadn't","half","happens","hardly","has","hasn't","have","haven't","having","he","he'd","he'll","hello","help","hence","her","here","hereafter","hereby","herein","here's","hereupon","hers","herself","he's","hi","him","himself","his","hither","hopefully","how","howbeit","however","hundred","i'd","ie","if","ignored","i'll","i'm","immediate","in","inasmuch","inc","inc.","indeed","indicate","indicated","indicates","inner","inside","insofar","instead","into","inward","is","isn't","it","it'd","it'll","its","it's","itself","i've","just","k","keep","keeps","kept","know","known","knows","last","lately","later","latter","latterly","least","less","lest","let","let's","like","liked","likely","likewise","little","look","looking","looks","low","lower","ltd","made","mainly","make","makes","many","may","maybe","mayn't","me","mean","meantime","meanwhile","merely","might","mightn't","mine","minus","miss","more","moreover","most","mostly","mr","mrs","much","must","mustn't","my","myself","name","namely","nd","near","nearly","necessary","need","needn't","needs","neither","never","neverf","neverless","nevertheless","new","next","nine","ninety","no","nobody","non","none","nonetheless","noone","no-one","nor","normally","not","nothing","notwithstanding","novel","now","nowhere","obviously","of","off","often","oh","ok","okay","old","on","once","one","ones","one's","only","onto","opposite","or","other","others","otherwise","ought","oughtn't","our","ours","ourselves","out","outside","over","overall","own","particular","particularly","past","per","perhaps","placed","please","plus","possible","presumably","probably","provided","provides","que","quite","qv","rather","rd","re","really","reasonably","recent","recently","regarding","regardless","regards","relatively","respectively","right","round","said","same","saw","say","saying","says","second","secondly","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sensible","sent","serious","seriously","seven","several","shall","shan't","she","she'd","she'll","she's","should","shouldn't","since","six","so","some","somebody","someday","somehow","someone","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specified","specify","specifying","still","sub","such","sup","sure","take","taken","taking","tell","tends","th","than","thank","thanks","thanx","that","that'll","thats","that's","that've","the","their","theirs","them","themselves","then","thence","there","thereafter","thereby","there'd","therefore","therein","there'll","there're","theres","there's","thereupon","there've","these","they","they'd","they'll","they're","they've","thing","things","think","third","thirty","this","thorough","thoroughly","those","though","three","through","throughout","thru","thus","till","to","together","too","took","toward","towards","tried","tries","truly","try","trying","t's","twice","two","un","under","underneath","undoing","unfortunately","unless","unlike","unlikely","until","unto","up","upon","upwards","us","use","used","useful","uses","using","usually","v","value","various","versus","very","via","viz","vs","want","wants","was","wasn't","way","we","we'd","welcome","well","we'll","went","were","we're","weren't","we've","what","whatever","what'll","what's","what've","when","whence","whenever","where","whereafter","whereas","whereby","wherein","where's","whereupon","wherever","whether","which","whichever","while","whilst","whither","who","who'd","whoever","whole","who'll","whom","whomever","who's","whose","why","will","willing","wish","with","within","without","wonder","won't","would","wouldn't","yes","yet","you","you'd","you'll","your","you're","yours","yourself","yourselves","you've","zero","a","how's","i","when's","why's","b","c","d","e","f","g","h","j","l","m","n","o","p","q","r","s","t","u","uucp","w","x","y","z","I","www","amount","bill","bottom","call","computer","con","couldnt","cry","de","describe","detail","due","eleven","empty","fifteen","fifty","fill","find","fire","forty","front","full","give","hasnt","herse","himse","interest","itse”","mill","move","myse”","part","put","show","side","sincere","sixty","system","ten","thick","thin","top","twelve","twenty","abst","accordance","act","added","adopted","affected","affecting","affects","ah","announce","anymore","apparently","approximately","aren","arent","arise","auth","beginning","beginnings","begins","biol","briefly","ca","date","ed","effect","et-al","ff","fix","gave","giving","heres","hes","hid","home","id","im","immediately","importance","important","index","information","invention","itd","keys","kg","km","largely","lets","line","'ll","means","mg","million","ml","mug","na","nay","necessarily","nos","noted","obtain","obtained","omitted","ord","owing","page","pages","poorly","possibly","potentially","pp","predominantly","present","previously","primarily","promptly","proud","quickly","ran","readily","ref","refs","related","research","resulted","resulting","results","run","sec","section","shed","shes","showed","shown","showns","shows","significant","significantly","similar","similarly","slightly","somethan","specifically","state","states","stop","strongly","substantially","successfully","sufficiently","suggest","thered","thereof","therere","thereto","theyd","theyre","thou","thoughh","thousand","throug","til","tip","ts","ups","usefully","usefulness","'ve","vol","vols","wed","whats","wheres","whim","whod","whos","widely","words","world","youd","youre"
-]
-EXTRA_STOP_WORDS = MORE_STOP_WORDS + EVEN_MORE_STOP_WORDS
-
 EXTENSION = ".csv"
 
 CURRENT_PATH: Path = str(Path(__file__).parent.resolve())
@@ -36,16 +32,21 @@ class nlp_data_ops:
     
     def remove_specific_text(self,df):
         #ONLY CALL THIS FUNCTION IF YOU ARE USING THE "raw_yelp_review_data.csv" FILE
+        df['full_review_text'] = [new_text[8:] for new_text in df['full_review_text']]
         df['full_review_text'] = [new_text.replace("check-in","") for new_text in df['full_review_text']]
         df['full_review_text'] = [new_text.lstrip('0123456789.- ') for new_text in df['full_review_text']]
+        df['full_review_text'] = [new_text.lstrip('s') for new_text in df['full_review_text']]
+
+        if type(df['star_rating'][0]) == np.int64:
+            return
+        else:
+            df['star_rating'] = df['star_rating'].str[:2]
+            df['star_rating'] = [int(rating) for rating in df['star_rating']]
 
     def perform_pre_processing(self,text):
         text = text.lower()
         text = text.translate(str.maketrans('', '', string.punctuation))
         tokens = nltk.word_tokenize(text)
-        stop_words = set(stopwords.words('english'))
-        tokens = [token for token in tokens if token.lower() not in stop_words]
-        tokens = [token for token in tokens if token.lower() not in EXTRA_STOP_WORDS]
         tokens = nltk.pos_tag(tokens)
         lemmatizer = WordNetLemmatizer()
         tokens = [lemmatizer.lemmatize(token[0]) for token in tokens]
@@ -75,15 +76,11 @@ class nlp_data_ops:
         df['str_sent'] = review_sentiment
         df['sent_polarity'] = sent_polarity
 
-
-import tkinter as tk
-from tkinter import simpledialog
-
 ROOT = tk.Tk()
 
 ROOT.withdraw()
 # the input dialog
-USER_INP = simpledialog.askstring(title="Corpus Maker",
+USER_INP = simpledialog.askstring(title="Text Pre-Processor",
                                   prompt="Enter the name of the file (without the file extension) you want to pre-process:")
 
 print("Processing Text...")
@@ -91,7 +88,7 @@ df = pd.read_csv(CURRENT_PATH + "\\" + USER_INP + EXTENSION)
 pipeline = nlp_data_ops()
 df = pipeline.perform_cleaning(df)
 ## Uncomment this line if using the "raw_yelp_review_data" file.
-#pipeline.remove_specific_text(df)
+pipeline.remove_specific_text(df)
 corpus = [pipeline.perform_pre_processing(review_corpus) for review_corpus in df['full_review_text']]
 pipeline.save_sent(df)
 df['full_review_text'] = corpus
