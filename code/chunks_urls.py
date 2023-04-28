8d7f09fdf0104dfb10e1739502bb90efffd173a9
popusa <danielmaged.3@gmail.com>
2023-04-28 14:57:59 +0300

    fixed missing chunk issue

--- code/chunks_urls.py
+++ code/chunks_urls.py
@@ -1,3 +1,3 @@
 base = 'https://f005.backblazeb2.com/file/yelp-review-data/chunk_'
 end_of_file = '.csv'
-CHUNKS_URLS = [base + str(num) + end_of_file for num in range(1,60)]
+CHUNKS_URLS = [base + str(num) + end_of_file for num in range(1,61)]
